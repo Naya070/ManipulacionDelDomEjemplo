9f6b2e57524f3903292ee757f42c31b32529f782
Naya070 <nayarith070@gmail.com>
2023-01-05 09:07:09 -0400

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,15 @@
+//h1 {color:red}
+//.parrafito {...} 
+//#pid {...}
+
+const h1 = document.querySelector('h1')
+const input1 = document.querySelector('#calculo1')
+const input2 = document.querySelector('#calculo2')
+const btn = document.querySelector('#btnCalcular')
+const pResult = document.querySelector('#result')
+
+
+function btnOnClick() {
+    const sumaInputs = parseFloat(input1.value) + parseFloat(input2.value);
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
